ad04769a750971b7485bb2bf514e8895f73b9156
Woody Butler <forrestwbutler@gmail.com>
2015-07-15 16:41:45 -0400

    chanigng to chalries graycsale

---
+++ js/starfill.js
@@ -0,0 +1,16 @@
+/* starfill.js writes a number of blue stars, which is the lone argument (up to 5) and fills the rest with gray stars.
+This is used to create a rating system, currently implemented in Chalrie Butler's online resume.
+
+By Woody Butler
+Decemeber 2014
+*/
+
+function starfill(skill) {
+	document.getElementbyId
+	if (skill > 5)
+		skill =5;
+	for (i=0; i<skill; i++){
+
+	}
+
+}
